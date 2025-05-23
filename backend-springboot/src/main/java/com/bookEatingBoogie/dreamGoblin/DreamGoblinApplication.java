fd37ebdb71f4daeed9e0109c6526c5cc8a2340ab
leeseoyoung <moosim1120@gmail.com>
2025-05-23 23:57:44 +0900

    fix: remove submodule and add actual springboot code

---
+++ backend-springboot/src/main/java/com/bookEatingBoogie/dreamGoblin/DreamGoblinApplication.java
@@ -0,0 +1,14 @@
+package com.bookEatingBoogie.dreamGoblin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class DreamGoblinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamGoblinApplication.class, args);
+	}
+
+}
